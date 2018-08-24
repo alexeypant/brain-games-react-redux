ab9ab7b72fd4dff1f39c40752f5184d645016373
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-08-24 21:32:01 +0300

    App js removed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import reducers from './reducers';
 import AppContainer from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-
-
 
 const store = createStore(
     reducers,
